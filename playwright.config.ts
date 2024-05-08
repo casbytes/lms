82ae4ea8b18dca182f1878cb748db5edec250afd
Christopher A. Sesugh <codingsimba@Christophers-MacBook-Pro.local>
2024-05-08 17:16:51 +0100

    test: update playwright config

--- playwright.config.ts
+++ playwright.config.ts
@@ -10,7 +10,7 @@ export default defineConfig({
   workers: process.env.CI ? 1 : undefined,
   reporter: "html",
   use: {
-    baseURL: "http://127.0.0.1:5173",
+    baseURL: "http://localhost:3000",
     trace: "on-first-retry",
   },
 
@@ -53,7 +53,7 @@ export default defineConfig({
 
   webServer: {
     command: "npm run dev",
-    url: "http://127.0.0.1:5173",
+    url: "http:localhost:3000",
     reuseExistingServer: !process.env.CI,
   },
 });
